cfb681de1e63a297047c6494bc194c1dcd32246e
blondie07 <44232509+blondie07@users.noreply.github.com>
2019-04-19 18:19:27 +0200

    maj fin seance 1

--- TP1/src/TP1.cpp
+++ TP1/src/TP1.cpp
@@ -20,13 +20,26 @@ using namespace std;
 
 int main() {
 //creation d'un tableau de figures fermees
-
+cout << "coucou" << endl;
 vector<Figure*> vect;
 
 Carre car1(5);
 TriangleEquilateral tri1(4);
 Cercle cer1(6);
+Rectangle rec1(4,6);
 
+cout << "coucou1.5" << endl;
 
+vect[0]= &car1;
+vect[1]= &tri1;
+vect[2]= &cer1;
+vect[3]= &rec1;
+
+cout << "coucou2" << endl;
+for(int i = 0; i <= 3; i++){
+	cout << "Figure numéro " << i << " : ";
+	vect[i]->perimetre();
+	vect[i]->afficherCaracteristiques();
+}
 
 }
